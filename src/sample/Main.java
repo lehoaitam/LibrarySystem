211f9b1bc942329b951633c9177ae97a7f44aea6
985261 <dtakele@gmail.com>
2016-06-01 14:49:38 -0500

    change title again

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Library System");
+        primaryStage.setTitle("Library System 1");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
