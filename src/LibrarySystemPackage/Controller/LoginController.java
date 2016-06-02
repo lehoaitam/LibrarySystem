81ef9f6223e537270e60eb4f7a7ff9b9670857dc
985261 <dtakele@gmail.com>
2016-06-02 10:30:23 -0500

    Login window is added

---
+++ src/LibrarySystemPackage/Controller/LoginController.java
@@ -0,0 +1,15 @@
+package LibrarySystemPackage.Controller;
+
+import javafx.event.Event;
+
+/**
+ * Created by lehoaitam on 6/1/16.
+ */
+public class LoginController {
+
+    public void handelCancelRegisterMember(Event event) {
+    }
+
+    public void handelRegisterMember(Event event) {
+    }
+}
