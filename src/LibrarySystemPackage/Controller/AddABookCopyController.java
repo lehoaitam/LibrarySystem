eda1473c431cafa899e44ec64345cac9f43d37b6
Sang Tran <minhsanguitx@gmail.com>
2016-06-02 12:00:38 -0500

    Included Add Book Copy Controller

---
+++ src/LibrarySystemPackage/Controller/AddABookCopyController.java
@@ -0,0 +1,16 @@
+package LibrarySystemPackage.Controller;
+
+import LibrarySystemPackage.AbstractView;
+
+/**
+ * Created by 985259 on 6/2/2016.
+ */
+public class AddABookCopyController extends AbstractView{
+
+    public AddABookCopyController()
+    {
+        this.title = "Add a book copy";
+        this.viewPath = "../View/AddABookCopyView.fxml";
+    }
+
+}
