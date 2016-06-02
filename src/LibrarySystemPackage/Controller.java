e9b27573a29b6211652c8fe65c4a8a44fbedc1ac
lehoaitam <lehoaitam@gmail.com>
2016-06-01 23:28:13 -0500

    Change main interface Add controller to handle control event

--- src/LibrarySystemPackage/Controller.java
+++ src/LibrarySystemPackage/Controller.java
@@ -1,4 +1,35 @@
 package LibrarySystemPackage;
 
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Controller {
+    public void handleRegisterMemberButtonAction(Event event) {
+        try {
+            //tam code
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/RegisterUser.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Register User");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handelLoginButtonAction(Event event) {
+        //dtakele code
+    }
+
+    public void handelCheckOutButtonAction(Event event) {
+        //esaago code
+    }
+
+    public void handleAddCopyBookButton(Event event) {
+        //sang code
+    }
 }
