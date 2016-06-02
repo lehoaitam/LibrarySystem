3443398fdfb6b2b22006ebf9d66da2aa19e0f24a
985261 <dtakele@gmail.com>
2016-06-02 13:32:34 -0500

    Login window modified, and script and stylesheet folders added

--- src/LibrarySystemPackage/Controller/LoginController.java
+++ src/LibrarySystemPackage/Controller/LoginController.java
@@ -1,15 +1,25 @@
 package LibrarySystemPackage.Controller;
 
 import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 /**
  * Created by lehoaitam on 6/1/16.
  */
 public class LoginController {
 
-    public void handelCancelRegisterMember(Event event) {
-    }
+    @FXML
+    private TextField loginUserName;
+    @FXML
+    private PasswordField loginPWRD;
 
-    public void handelRegisterMember(Event event) {
+    public void handelLoginUser(Event event) {
+String userName=loginUserName.getText();
+        String pwrd = loginPWRD.getText();
+
+        System.out.println(userName);
+        System.out.println(pwrd);
+
     }
 }
