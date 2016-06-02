7f93f17ae21f1f24c73d6d5ce2ab3fb461cf9891
lehoaitam <lehoaitam@gmail.com>
2016-06-01 20:18:59 -0500

    Chage package name

---
+++ src/LibrarySystemPackage/Controller.java
@@ -0,0 +1,4 @@
+package LibrarySystemPackage;
+
+public class Controller {
+}
--- src/LibrarySystemPackage/Main.java
+++ src/LibrarySystemPackage/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package LibrarySystemPackage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
