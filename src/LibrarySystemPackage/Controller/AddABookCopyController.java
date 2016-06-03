576cf2c49537869e2de4f2a023c8c8b397e3ea6d
Sang Tran <minhsanguitx@gmail.com>
2016-06-03 10:35:04 -0500

    Call showView function from Add Book Copy Controller

--- src/LibrarySystemPackage/Controller/AddABookCopyController.java
+++ src/LibrarySystemPackage/Controller/AddABookCopyController.java
@@ -5,10 +5,9 @@ import LibrarySystemPackage.AbstractView;
 /**
  * Created by 985259 on 6/2/2016.
  */
-public class AddABookCopyController extends AbstractView{
+public class AddABookCopyController extends AbstractView {
 
-    public AddABookCopyController()
-    {
+    public AddABookCopyController() {
         this.title = "Add a book copy";
         this.viewPath = "../View/AddABookCopyView.fxml";
     }
