4effa61a32254bd881d5bbd16b6b09b79b0ba8e3
Sang Tran <minhsanguitx@gmail.com>
2016-06-03 14:36:02 -0500

    Call showView function from Add Book Copy Controller

--- src/LibrarySystemPackage/Controller/AddABookCopyController.java
+++ src/LibrarySystemPackage/Controller/AddABookCopyController.java
@@ -1,15 +1,35 @@
 package LibrarySystemPackage.Controller;
 
 import LibrarySystemPackage.AbstractView;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.*;
+
 
 /**
  * Created by 985259 on 6/2/2016.
  */
 public class AddABookCopyController extends AbstractView {
 
+    @FXML
+    private TextField txtISBN;
+
+    @FXML
+    private Button btnCancel;
+
     public AddABookCopyController() {
         this.title = "Add a book copy";
         this.viewPath = "../View/AddABookCopyView.fxml";
     }
 
+    public void btnSearchClicked(){
+        System.out.print(txtISBN.getText());
+    }
+
+    public void btnOkClicked(){
+
+    }
+    public void btnCancelClicked(){
+        ((Stage) btnCancel.getScene().getWindow()).close();
+    }
 }
